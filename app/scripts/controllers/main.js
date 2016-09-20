10b8c548a7e1379b34e4d7c9551ecf9ecb026940
Summer-Dong <D__yanan@163.com>
2016-09-20 21:52:50 +0800

    [#10]fixed #10

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -113,7 +113,6 @@ angular.module('snapshotApp')
 					}
 				}
 			}
-			console.log(nodesFinal);
 
 			//判断输入id所位于的结点，如果没有找到，报错。
 			var idIndex;
@@ -130,6 +129,42 @@ angular.module('snapshotApp')
 				return;
 			}
 
+			// 计算动物在所输入时间的位置
+			var animalStatus = {};
+			//遍历到对所查询时间
+			for (var i = 0; i <= idIndex; i++) {
+				// var tt=nodesFinal[i].animal;
+				//遍历单个节点中所有动物
+				for (var j in nodesFinal[i].animal) {
+					//动物出现过
+					if (animalStatus.hasOwnProperty(j)) {
+						//动物上条记录所在的位置
+						var lastX = animalStatus[j].x;
+						var lastY = animalStatus[j].y;
+						//累加的坐标与实际坐标（所查询时间的上条所在的坐标）不符合
+						if (lastX != nodesFinal[i].animal[j].origX || lastY != nodesFinal[i].animal[j].origY) {
+							console.log("Conflict found at " + id);
+							return;
+						}
+						// 符合实际时，累加坐标值
+						animalStatus[j].x += nodesFinal[i].animal[j].x;
+						animalStatus[j].y += nodesFinal[i].animal[j].y;
+					} else {
+						//有新动物时添加记录
+						animalStatus[j] = {};
+						animalStatus[j]['x'] = nodesFinal[i].animal[j].origX;
+						animalStatus[j]['y'] = nodesFinal[i].animal[j].origY;
+
+						//新动物进入标记区域时，却在结点记录中有坐标变化量'x'或'y'时，则此条记录出错
+						if (('x' in nodesFinal[i].animal[j]) || ('y' in nodesFinal[i].animal[j])) {
+							vm.result = "Conflict found at " + id;
+							console.log("Conflict found at " + id);
+							return;
+						}
+					}
+				}
+			}
+ 
 		};
 
 
