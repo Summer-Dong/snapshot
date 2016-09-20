76832a8830f2ca35a5358600837e9d0cb3fece8f
Summer-Dong <D__yanan@163.com>
2016-09-20 21:54:50 +0800

    [#11]fixed #11

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -165,8 +165,15 @@ angular.module('snapshotApp')
 				}
 			}
  
+ 			//按照动物名字排序输出
+			var aniStaInOrder = [];
+			for (var i in animalStatus) {
+				aniStaInOrder.push(i);
+			}
+			aniStaInOrder.sort();
+			for (var i = 0; i < aniStaInOrder.length; i++) {
+				vm.result+=(aniStaInOrder[i] + " " + animalStatus[aniStaInOrder[i]].x + " " + animalStatus[aniStaInOrder[i]].y + "\n");
+			}
 		};
-
-
 
 	});
