b59da35e8dc90564cbd234e6d5a9477632fec82f
Summer-Dong <D__yanan@163.com>
2016-09-21 16:04:20 +0800

    [#16]fixed #16

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('snapshotApp')
-	.controller('MainCtrl', ['dataToGroup', 'splitNodes', function(dataToGroup, splitNodes) {
+	.controller('MainCtrl', ['dataToGroup', 'splitNodes','sortByTime', function(dataToGroup, splitNodes, sortByTime) {
 		var vm = this;
 		vm.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee 2016/09/02 22:30:46 cat1 10 9 351055db-33e6-4f9b-bfe1-16f1ac446ac1 2016/09/02 22:30:52 cat1 10 9 2 -1 cat2 2 3 dcfa0c7a-5855-4ed2-bc8c-4accae8bd155 2016/09/02 22:31:02 cat1 12 8 3 4";
 		vm.ID = "";
 		vm.result = "";
-
 		
-		vm.getSnapshot = function(historyData, id) {
+		vm.getSnapshot = function( ) {
 			/*将historyData拆分成nodes数组*/
 			dataToGroup.getNodes(vm.historyData)
 				.then(function(result){
+					//拆分每个结点
 					return splitNodes.splitNodes(result);
+				})
+				.then(function(result) {
+					//按照时间对结点冒泡排序
+					return sortByTime.bubbleSort(result);
 				})
 				.then(function(result) {
 					vm.nodesFinal = result;
@@ -22,27 +26,10 @@ angular.module('snapshotApp')
 			
 
 
-
-			/*拆分每个结点*/
-			// vm.nodesFinal = splitNodes.splitNodes(vm.nodes);
-			// console.log(vm.nodesFinal);
-
-
 		};
 
 	}]);
 
-// //按照时间对结点冒泡排序
-// for (var i = nodesFinal.length - 1; i >= 1; i--) {
-// 	for (var j = 0; j < i; j++) {
-// 		var tmp;
-// 		if (nodesFinal[j].time > nodesFinal[j + 1].time) {
-// 			tmp = nodesFinal[j];
-// 			nodesFinal[j] = nodesFinal[j + 1];
-// 			nodesFinal[j + 1] = tmp;
-// 		}
-// 	}
-// }
 
 // //判断输入id所位于的结点，如果没有找到，报错。
 // var idIndex;
---
+++ app/scripts/services/sortByTime.js
@@ -0,0 +1,20 @@
+angular.module('snapshotApp')
+	.service('sortByTime', ['$q', 'values', function($q, values) {
+		var self = this;
+		self.bubbleSort = function(nodesFinal) {
+			var deferred = $q.defer();
+			//按照时间对结点冒泡排序
+			for (var i = nodesFinal.length - 1; i >= 1; i--) {
+				for (var j = 0; j < i; j++) {
+					var tmp;
+					if (nodesFinal[j].time > nodesFinal[j + 1].time) {
+						tmp = nodesFinal[j];
+						nodesFinal[j] = nodesFinal[j + 1];
+						nodesFinal[j + 1] = tmp;
+					}
+				}
+			}
+			deferred.resolve(nodesFinal);
+			return deferred.promise;
+		};
+	}]);
