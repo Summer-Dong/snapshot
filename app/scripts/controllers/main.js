7d5b66602509096a6076d1d0b45babf1f38ea2f7
Summer-Dong <D__yanan@163.com>
2016-09-22 10:10:35 +0800

    [#21]add mainctrl's unit test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,7 +14,8 @@ angular.module('snapshotApp')
 
 			//拆分每个结点
 			splitNodes.splitNodes(dataToGroup.nodes);
-			vm.result = (vm.result != "" ? vm.result : splitNodes.result);	
+			vm.result = (vm.result != "" ? vm.result : splitNodes.result);
+			if(vm.result != "")	{return;}
 			
 			//按照时间对结点冒泡排序
 			sortByTime.bubbleSort(splitNodes.nodesFinal);
@@ -22,10 +23,12 @@ angular.module('snapshotApp')
 			//对输入的id验证合法性
 			idVerify.idVerify(sortByTime.nodesFinal, vm.ID);
 			vm.result = (vm.result != "" ? vm.result : idVerify.result);
+			if(vm.result != "")	{return;}
 					
 			//计算动物在所输入时间的位置
 			calcuPos.position(sortByTime.nodesFinal, vm.ID);
 			vm.result = (vm.result != "" ? vm.result : calcuPos.result);
+			if(vm.result != "")	{return;}
 			
 			/*按照动物名字排序输出*/
 			sortByAnimalName.sortByName(calcuPos.animalStatus);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,17 +6,30 @@ describe('Controller: MainCtrl', function() {
     beforeEach(module('snapshotApp'));
 
     var MainCtrl, scope;
-
-    // Initialize the controller and a mock scope
+    
     beforeEach(inject(function($controller, $rootScope) {
         scope = $rootScope.$new();
-        MainCtrl = $controller('MainCtrl', {
+        MainCtrl = $controller('MainCtrl', function() {
             $scope: scope
-                // place here mocked dependencies
         });
     }));
 
-    it('should attach a list of awesomeThings to the scope', function() {
-        // expect(MainCtrl.awesomeThings.length).toBe(3);
+    it('should show the right results for the rigt inputs of historyData and ID', function() {
+        // when 
+        MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 2 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 8 3 4";
+        MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
+        MainCtrl.getSnapshot();
+        // then
+        expect(MainCtrl.result).toBe("cat1 15 12\ncat2 2 3\n");
     });
+
+    it('should show the wrong results for the wrong inputs of historyData and ID', function() {
+        // when 
+        MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 8 3 4";
+        MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
+        MainCtrl.getSnapshot();
+        // then
+        expect(MainCtrl.result).toBe("Invalid format.");
+    });
+    
 });
