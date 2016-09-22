3aa09363bc6e11bdea8fe288e058cee8a414b7a7
Summer-Dong <D__yanan@163.com>
2016-09-22 18:55:23 +0800

    [#20]fix CI bug

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,7 +19,7 @@ describe('Controller: MainCtrl', function() {
         // when 
         MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 2 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 8 3 4";
         MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
-        MainCtrl.getSnapshot(historyData, id);
+        MainCtrl.getSnapshot(MainCtrl.historyData, MainCtrl.ID);
         // then
         expect(MainCtrl.result).toBe("cat1 15 12\ncat2 2 3\n");
     });
@@ -29,7 +29,7 @@ describe('Controller: MainCtrl', function() {
         // when 
         MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 8 3 4";
         MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
-        MainCtrl.getSnapshot(historyData, id);
+        MainCtrl.getSnapshot(MainCtrl.historyData, MainCtrl.ID);
         // then
         expect(MainCtrl.result).toBe("Invalid format.");
     });
@@ -39,7 +39,7 @@ describe('Controller: MainCtrl', function() {
         // when 
         MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 2 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 7 3 4";
         MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
-        MainCtrl.getSnapshot(historyData, id);
+        MainCtrl.getSnapshot(MainCtrl.historyData, MainCtrl.ID);
         // then
         expect(MainCtrl.result).toBe("Conflict found at\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155.");
     });
@@ -49,7 +49,7 @@ describe('Controller: MainCtrl', function() {
         // when 
         MainCtrl.historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n2016/09/02 22:30:46\ncat1 10 9\n351055db-33e6-4f9b-bfe1-16f1ac446ac1\n2016/09/02 22:30:52\ncat1 10 9 2 -1\ncat2 2 3\ndcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n2016/09/02 22:31:02\ncat1 12 8 3 4";
         MainCtrl.ID = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd125";
-        MainCtrl.getSnapshot(historyData, id);
+        MainCtrl.getSnapshot(MainCtrl.historyData, MainCtrl.ID);
         // then
         expect(MainCtrl.result).toBe("Invalid id.");
     });
