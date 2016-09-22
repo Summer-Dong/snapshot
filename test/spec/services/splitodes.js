5445e8fc9d6c364b89028d87cce5a18b3bbbd42f
Summer-Dong <D__yanan@163.com>
2016-09-22 16:21:43 +0800

    [#20]try to fix CI bug

--- test/spec/services/splitodes.js
+++ test/spec/services/splitodes.js
@@ -17,8 +17,7 @@ describe('Unit Test of Service: splitNodes', function() {
 		splitNodes.splitNodes(nodes);
 		// then
 		expect(splitNodes.nodesFinal[0].id).toBe('e4e87cb2-8e9a-4749-abb6-26c59344dfee');
-		expect(splitNodes.nodesFinal[0].time).toBe(1472826646000);
-		// expect(splitNodes.nodesFinal[0].animal['cat1']['origX']).toBe(10);
-		// expect(splitNodes.nodesFinal[0].animal['cat1']['origY']).toBe(9);
+		expect(splitNodes.nodesFinal[0].animal['cat1']['origX']).toBe(10);
+		expect(splitNodes.nodesFinal[0].animal['cat1']['origY']).toBe(9);
 	});
 });
