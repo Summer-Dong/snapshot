bfb58ec584ffec3cb24252931403f7f791f32405
Summer-Dong <D__yanan@163.com>
2016-09-22 11:03:46 +0800

    [#20]处理字符串中的空白符

--- app/scripts/services/dataToGroup.js
+++ app/scripts/services/dataToGroup.js
@@ -4,6 +4,13 @@ angular.module('snapshotApp')
 
 		/*将historyData拆分成nodes数组*/
 		self.getNodes = function(historyData) {
+			// 替换字符串中首尾的空白符
+			historyData.replace(/(^\s*)|(\s*$)/g, "");
+			// 对字符串按照所有类型的空白字符进行分割，得到数组
+			var tempData = historyData.split(/\s+/g);
+			// 将得到的数组拼接成空格连接的字符串
+			historyData = tempData.join(" ");
+
 			// 获得时间组成的数组
 			var timeArray = historyData.match(values.timeRegExp);
 			// 获得时间id的长度
@@ -15,10 +22,10 @@ angular.module('snapshotApp')
 			for (var i = 0; i < timeArray.length - 1; i++) {
 				var start = historyData.indexOf(timeArray[i]) - idLength;
 				var end = historyData.indexOf(timeArray[i + 1]) - idLength;
-				self.nodes.push(historyData.substring(start, end - 1).replace(/(^\s*)|(\s*$)/g, ""));
+				self.nodes.push(historyData.substring(start, end - 1));
 			}
 			//单独处理i在末尾的情况
 			var start = historyData.indexOf(timeArray[i]) - idLength;
-			self.nodes.push(historyData.substring(start).replace(/(^\s*)|(\s*$)/g, ""));
+			self.nodes.push(historyData.substring(start));
 		};
 	}]);
