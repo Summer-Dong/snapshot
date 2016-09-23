d423df83fe94080e8f2bbf0c0bd0ab1079eb7161
Summer-Dong <D__yanan@163.com>
2016-09-23 08:44:48 +0800

    Update values.js

--- app/scripts/services/values.js
+++ app/scripts/services/values.js
@@ -1,5 +1,5 @@
 angular.module('snapshotApp')
-	.value]('values', function( ) {
+	.value('values', function( ) {
 		var self = this;
 		self.timeRegExp = RegExp(/[1-9][0-9]{3}\/[0-1][0-9]\/[0-3][0-9] [0-9]{2}:[0-9]{2}:[0-9]{2}/g);
 		self.error = "Invalid format.";
